7ae96a2f40f66b9b81494a5efe234f5e8838ef43
Vance Villanueva <vancervillanueva@gmail.com>
2017-10-03 15:05:04 -0700

    updated 100317

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
+const User = require('./user.js');
+const bycrypt = require('bcrypt');
 
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
@@ -12,8 +14,10 @@ server.use(session({
   secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
 }));
 
-/* Sends the given err, a string or an object, to the client. Sets the status
- * code appropriately. */
+
+
+// /* Sends the given err, a string or an object, to the client. Sets the status
+//  * code appropriately. */
 const sendUserError = (err, res) => {
   res.status(STATUS_USER_ERROR);
   if (err && err.message) {
@@ -23,12 +27,120 @@ const sendUserError = (err, res) => {
   }
 };
 
-// TODO: implement routes
+// EnsureLogin
+const ensureLogin = (req, res, next) => {
+  const { username } = req.session;
+  if (!username) {
+    sendUserError('must be logged in', res);
+    return;
+  }
 
-// TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      sendUserError(err, res);
+      return;
+    }
+
+    if (!user) {
+      sendUserError('must be logged in', res);
+      return;
+    }
+
+    req.user = user;
+    next();
+  });
+};
+
+
+// Post /users
+server.post('/users', (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    sendUserError('username and password are required', res);
+    return;
+  }
+
+  bcrypt.hash(password, BCRYPT_COST, (hashErr, newHash) => {
+    if (hashErr) {
+      sendUserError(hashErr, res);
+      return;
+    }
+
+    const user = new User({
+      username,
+      passwordHash: newHash
+    });
+
+    user.save((err, newUser) => {
+      if (err) {
+        sendUserError(err, res);
+        return;
+      }
+      res.json(newUser);
+    });
+  });
+});
+
+
+server.post('/log-in', (req, res) => {
+	const { username, password } = req.body;
+
+	if (!username || !password) {
+	   sendUserError('username and pasword are required', res);
+	   return;
+	}
+
+    User.findOne( { username }, (err, user) => {
+        if (err) {
+           sendUserError(err, res);
+           return;
+        }
+
+        if (!user) {
+           sendUserError('bad credentials', res);
+	       return;
+        }
+
+        bcrypt.compare(password, user.passwordHash, (compareErr, valid) => {
+            if (compareErr) {
+                  sendUserError(compareErr, res);
+	              return;
+            }
+        
+            if (!value) {
+              sendUserError('bad credentials', res);
+	          return;
+            }
+
+            req.session.username = username; // at the top
+            req.json({ success: true });
+        });
+    }); 
+});
+
+
+
+// Restrict Acceess
+const restrictAccess = (req, res, next) => {
+  const path = req.path;
+  if (/restricted/.test(path)) {
+    if (!req.session.username) {
+      sendUserError('Must be logged in to access', res);
+      return;
+    }
+  }
+  next();
+};
+
+server.use(restrictAccess);
+
+
+
+server.get('/me', ensureLogin, (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
 });
 
 module.exports = { server };
+
